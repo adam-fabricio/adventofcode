e726f335cc06c22d2598846abd79648f66e9f78e
adam <adamfabricio@gmail.com>
2023-12-04 18:13:23 -0300

    update: second star

--- 2023/src/04.py
+++ 2023/src/04.py
@@ -18,14 +18,21 @@ with open(path) as f:
     arquivo = f.read().splitlines()
 
 games = [ game.split(":")[1].split("|") for game in arquivo ]
+jogos = [ 1 for game in range(len(games)+1) ]
 
 ans = 0
-for game in games:
+for i, game in enumerate(games, 1):
     points = 0
     for number in game[1].strip().split():
         if number in game[0].strip().split():
             points += 1
     if points >= 1:
         ans += 2 ** (points - 1)
+        for dup in range(i+1, i+points+1):
+            if dup >= len(jogos):
+                continue
+            jogos[dup] += jogos[i]
 
-print(ans)
+print("parte 1:", ans)
+
+print("parte 2:", sum(jogos[1:]))
