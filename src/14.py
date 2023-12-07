cffffae7a608eb3f17835eabd172331fc68a9ade
adam <adamfabricio@gmail.com>
2023-12-06 22:37:16 -0300

    update: day_14

--- src/14.py
+++ src/14.py
@@ -3,7 +3,7 @@
 import os
 
 
-nome_arquivo = "24_test.in"
+nome_arquivo = "24.in"
 local = "assets"
 part = "1"
 
@@ -40,18 +40,41 @@ menos_comum = sorted(map(int, itens.keys()))[0]
 print(f'parte 1: {mais_comum - menos_comum}')
 
 
-print(raw_template)
+letras = {}
+for letra in raw_template:
+    if letra in letras:
+        letras[letra] += 1
+    else:
+        letras[letra] = 1
 
-templates = [ raw_template ]
-a = []
-for i in range(20):
-    templates.append( ''.join(processo(list(templates[i]), instrucao)))
-    
-    if templates[i] in templates[i+1]:
-        break
+pares = {}
+for i in range(len(raw_template) - 1):
+    par = raw_template[i:i+2] 
+    if par in pares:
+        pares[par] += 1
+    else:
+        pares[par] = 1
 
-print(i)
 
-#print(templates[i] )
+for i in range(40):
+    pares_copy = pares.copy()
+    pares = {}
+    for par in pares_copy:
+        nova_letra = instrucao[par]
+        if nova_letra in letras:
+            letras[nova_letra] += pares_copy[par]
+        else:
+            letras[nova_letra] = pares_copy[par]
 
-#print(templates[i+1])
+        novo_pares = [par[0]+nova_letra, nova_letra+par[1]]
+        for n_p in novo_pares:
+            if n_p in pares:
+                pares[n_p] += pares_copy[par]
+            else:
+                pares[n_p] = pares_copy[par]
+
+
+num_letras = sorted(letras.values())
+
+print(f'parte_2 = {num_letras[-1] - num_letras[0] }')
+
