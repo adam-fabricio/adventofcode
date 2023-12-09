8310af289508e6abde0b9ba45af84480aacf22fb
adam <adamfabricio@gmail.com>
2023-12-09 17:39:34 -0300

    feat: second star day 8.

--- 2023/src/08.py
+++ 2023/src/08.py
@@ -23,16 +23,17 @@ dict_maps = { l.split(" ")[0]:
              { "L": l.split(" ")[2][1:-1], "R": l.split(" ")[3][:-1]} 
              for l in maps.splitlines() }
 
-#position = "AAA"
-#i = 0
-#while position != "ZZZ":
-#    index = i % len(inst)
-#    position = dict_maps[position][inst[index]]
-#    i += 1
-#print("PARTE 1:", i)
-#
-#
-positions = [ p for p in dict_maps.keys() if re.match("[0-9A-Z][0-9A-Z][A]", p ) ]
+position = "AAA"
+i = 0
+while position != "ZZZ":
+    index = i % len(inst)
+    position = dict_maps[position][inst[index]]
+    i += 1
+print("PARTE 1:", i)
+
+
+positions = [ p for p in dict_maps.keys()
+        if re.match("[0-9A-Z][0-9A-Z][A]", p ) ]
 ends = [ p for p in dict_maps.keys() if re.match("[0-9A-Z][0-9A-Z][Z]", p ) ]
 
 ans = []
@@ -42,15 +43,24 @@ for p in positions:
     while True:
         index = i % len(inst)
         p = dict_maps[p][inst[index]]
+        i += 1
         if p in ends:
             ans.append(i)
             break
-        i += 1
 
-print(ans)
-a = 1
-for a in ans:
-    a *= a
+mmc = 1
+d = 2
+while not all( a == 1 for a in ans):
+    c = 0
+    for i, a in enumerate(ans):
+        if a % d == 0:
+            ans[i] = a / d
+            if c:
+                continue
+            c = 1
+    if c:
+        mmc *= d
+        continue
+    d += 1
 
-print(a)
-
+print("parte 2:", mmc)
