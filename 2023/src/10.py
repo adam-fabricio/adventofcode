47ef1bc3ea5d16232539a6f05b66451c6b2a601d
Adam <afssilva@metrosp.com.br>
2023-12-11 01:47:04 -0300

    update: tentando ainda day 10

--- 2023/src/10.py
+++ 2023/src/10.py
@@ -20,10 +20,12 @@ path = os.path.join("data", dia)
 with open(path) as f:
     arquivo = f.read().splitlines()
 
+dot = set()
 mapa = {}
 for l, lines in enumerate(arquivo):
     for c, col in enumerate(lines):
         if col == ".":
+            dot.add( (l, c) )
             continue
         mapa[(l, c)] = []
         if col == "S":
@@ -65,16 +67,74 @@ for k in mapa.keys():
 print(start)
 print("="*80)
 
-def bfs(mapa:dict, local: tuple, visitados:set = set()) -> int:
+visitados = set()
+
+def dfs(mapa:dict, local: tuple) -> int:
     visitados.add( local )
     l, c = local
-    # print(local, "->",  mapa[local], arquivo[l][c])
     for vizinho in mapa[local]:
         if vizinho in visitados:
             continue
-        return bfs(mapa, vizinho, visitados) + 1
-    return 0
+        return dfs(mapa, vizinho) + 1
+    return 1
 
-print( (bfs(mapa, start) + 1) /2 )
-#6968
-# ulimit -s 100000000
+print( ( dfs(mapa, start) ) /2 )
+inner = set()
+out = set()
+for d in dot:
+    l, c = d
+    if l == 0 or c == 0 or c==(len(arquivo[0])-1) or l == (len(arquivo)-1):
+        out.add( (l, c) )
+        continue
+    for direcoes in [ (0,1), (1,0), (0,-1), (-1,0) ]:
+        dl, dc = direcoes
+        ll = l + dl
+        cc = c + dc
+        convert = 0
+        while True:
+            if (ll, cc) in out:
+                out.add( (l, c) )
+                convert = 1
+                break
+            elif (ll == 0 or cc == 0 or cc==(len(arquivo[0])-1)
+                  or ll == (len(arquivo)-1)):
+                out.add( (l, c) )
+                convert = 1
+                break
+            elif (ll, cc) in visitados:
+                inner.add( (l, c) )
+                convert = 1
+                break
+            ll += dl
+            cc += dc
+        if convert:
+            continue
+
+
+
+
+for l, lines in enumerate(arquivo):
+    for c, col in enumerate(lines):
+        if (l, c)  in visitados:
+            print("\033[0;32;40m", end="")
+            print(col, end="")
+            print("\033[0m", end="")
+        elif (l, c) in out:
+            print("\033[0;34;31m", end="")
+            print("O", end="")
+            print("\033[0m", end="")
+        elif (l, c) in inner:
+            print("\033[0;32;35m", end="")
+            print("I", end="")
+            print("\033[0m", end="")
+        elif (l, c) in dot:
+            print("\033[0;33;31m", end="")
+            print(col, end="")
+            print("\033[0m", end="")
+
+        else:
+            print(col, end ="")
+    print()
+
+#  6968
+#  ulimit -s 100000000
