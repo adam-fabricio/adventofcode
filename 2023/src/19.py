af5dfd4f39d9093a1387eca90a8960541c5ab918
Adam <afssilva@metrosp.com.br>
2023-12-21 04:37:43 -0300

    update: first star

--- 2023/src/19.py
+++ 2023/src/19.py
@@ -14,6 +14,43 @@ if test:
 
 path = os.path.join("data", dia)
 with open(path) as f:
-    arquivo = f.read()
+    arquivo = f.read().split("\n\n")
 
+workflow = {chave: [[item
+                     for item in v.split(":")]
+                    for v in valor[:-1].split(",") ]
+            for linha in arquivo[0].splitlines()
+            for chave, _, valor in [linha.partition("{")]}
 
+parts = [{r: int(val)
+          for item in linha[1:-1].split(",")
+          for r, val in [item.split('=') ]}
+         for linha in arquivo[1].splitlines()]
+
+ans = 0
+for n, part in enumerate(parts):
+    flow = "in"
+    while not flow in "AR":
+        flag = 0
+        for i in range(len(workflow[flow])-1):
+            r = workflow[flow][i][0][0]
+            op = workflow[flow][i][0][1]
+            val = int(workflow[flow][i][0][2:])
+            n_flow = workflow[flow][i][1]
+            if op == "<":
+                if part[r] < val:
+                    flow = n_flow
+                    flag = 1
+                    break
+            else:
+                if part[r] > val:
+                    flow = n_flow
+                    flag = 1
+                    break
+        if not flag:
+            flow = workflow[flow][-1][0]
+    if flow == "A":
+        for p in part:
+            ans += part[p]
+
+print(ans)
