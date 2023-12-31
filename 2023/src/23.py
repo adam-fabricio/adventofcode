156084c2e075950937f281f374cfdd939af47ff8
Adam <afssilva@metrosp.com.br>
2023-12-31 03:19:05 -0300

    update: duas estrelas, com ajuda do hyper neutrino

--- 2023/src/23.py
+++ 2023/src/23.py
@@ -3,8 +3,24 @@
 
 import os
 import re
-from sys import argv
+from sys import argv, setrecursionlimit
 from collections import deque
+from functools import cache
+
+
+setrecursionlimit(10**6)
+def dfs(vertice):
+    if vertice == final:
+        return 0
+
+    m=-3
+    visitados.add(vertice)
+    for n_vertice in vertices[vertice]:
+        if n_vertice not in visitados:
+            m = max(m, dfs(n_vertice) + vertices[vertice][n_vertice])
+            #print(m)
+    visitados.remove(vertice)
+    return m
 
 
 test = 0 if len(argv) == 2 else 1
@@ -37,7 +53,7 @@ vertices = {(l, c): {} for l, line in enumerate(arquivo)
         col != "#" and sum(1
             for n_l, n_c in [(l - 1, c), (l + 1, c), (l, c - 1), (l, c + 1)]
             if 0 <= n_l < l_max and 0 <= n_c < c_max
-            and arquivo[n_l][n_c] != "#") > 2}
+            and arquivo[n_l][n_c] != "#") != 2}
 
 
 for v_l, v_c in vertices:
@@ -46,9 +62,9 @@ for v_l, v_c in vertices:
 
     while fila:
         l, c, d = fila.popleft()
-        print(l, c)
-        if d != 0 and vertices.get((l, c), 0):
+        if d != 0 and (l, c) in vertices.keys():
             vertices[(v_l, v_c)][(l, c)] = d
+            vertices[(l, c)][(v_l, v_c)] = d
             continue
 
         for n_l, n_c in [(l+1, c), (l-1, c), (l, c+1), (l, c-1)]:
@@ -58,5 +74,5 @@ for v_l, v_c in vertices:
                     (n_l, n_c) not in visitados):
                 fila.append((n_l, n_c, d+1))
                 visitados.add((n_l, n_c))
-
-print(vertices)
+visitados = set()
+print(dfs(inicio))
